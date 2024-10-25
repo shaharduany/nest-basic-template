f481d6a745a5304095eee4062ae9a8186130d781
Shahar Duany <shahar.duany@gmail.com>
2024-10-25 22:49:14 +0300

    feat: first route for fetching all users

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './schemas/user.entity';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('')
+  public async getAllUsers(): Promise<User[]> {
+    return this.userService.getAllUsers();
+  }
+}
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './schemas/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  public async getAllUsers(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+}
