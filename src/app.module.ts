0d7361de44f6dcce597994c63dd60f221e5f61aa
Shahar Duany <shahar.duany@gmail.com>
2024-10-25 12:54:56 +0300

    feat: db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,8 @@ import { IHttpRequestConfigs } from '@common/interfaces/configurations.inteface'
 import { configurations } from '@app/configurations/configurations';
 import { UsersModule } from '@modules/users/users.module';
 import { HealthCheckModule } from '@modules/health-check/health-check.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DB_TYPE } from './common/constants/database';
 
 @Module({
   imports: [
@@ -23,6 +25,20 @@ import { HealthCheckModule } from '@modules/health-check/health-check.module';
         return ConfigService.get<IHttpRequestConfigs>('httpRequests');
       },
       inject: [ConfigService],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: DB_TYPE,
+        host: configService.get<string>('database.host'),
+        port: configService.get<number>('database.port'),
+        username: configService.get<string>('database.username'),
+        password: configService.get<string>('database.password'),
+        database: configService.get<string>('database.name'),
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
     }),
     UsersModule,
     HealthCheckModule,
